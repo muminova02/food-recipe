6a4b44054078ca2150b994ef924ce875660267eb
muminova_02 <muminova.2m@gmail.com>
2024-10-23 19:31:54 +0500

    innitial

---
+++ src/test/java/uz/doublem/foodrecipe/FoodRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.doublem.foodrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
