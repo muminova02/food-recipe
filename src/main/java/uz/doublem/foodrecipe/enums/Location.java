844869b61b396c7b35d07dedd14a9db55111d5a6
muminova_02 <muminova.2m@gmail.com>
2024-10-28 19:29:12 +0500

    entity same

---
+++ src/main/java/uz/doublem/foodrecipe/enums/Location.java
@@ -0,0 +1,33 @@
+package uz.doublem.foodrecipe.enums;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String city;
+
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "location", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<User> users;
+
+}
