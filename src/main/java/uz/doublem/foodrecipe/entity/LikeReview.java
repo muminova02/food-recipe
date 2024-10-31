d9a137d326c3c2ef3bdbbafdeeb38a709e6f51d9
muminova_02 <muminova.2m@gmail.com>
2024-10-31 17:08:48 +0500

    muq1

---
+++ src/main/java/uz/doublem/foodrecipe/entity/LikeReview.java
@@ -0,0 +1,29 @@
+package uz.doublem.foodrecipe.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class LikeReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    private Boolean isLike;
+
+}
