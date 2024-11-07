90b669ca2d679403e49522a5b768d64faeaad082
Xushnud <xushnudxurramov9@gmail.com>
2024-11-07 21:44:22 +0500

    Xushnud

---
+++ src/main/java/uz/doublem/foodrecipe/controller/SavedRecipeController.java
@@ -0,0 +1,34 @@
+package uz.doublem.foodrecipe.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import uz.doublem.foodrecipe.entity.Recipe;
+import uz.doublem.foodrecipe.entity.SavedRecipes;
+import uz.doublem.foodrecipe.service.SavedRecipeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/saved-recipes")
+public class SavedRecipeController {
+    final SavedRecipeService savedRecipeService;
+
+    @Autowired
+    public SavedRecipeController(SavedRecipeService savedRecipeService) {
+        this.savedRecipeService = savedRecipeService;
+    }
+
+    @PostMapping
+    public SavedRecipes save(@RequestBody Recipe recipe) {
+        return savedRecipeService.addRecipe(recipe);
+    }
+
+    @GetMapping
+    public List<SavedRecipes> getAllRecipesById(Integer ownerId) {
+        return savedRecipeService.getSavedReciepeById(ownerId);
+    }
+
+
+
+
+}
---
+++ src/main/java/uz/doublem/foodrecipe/entity/SavedRecipes.java
@@ -0,0 +1,23 @@
+package uz.doublem.foodrecipe.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SavedRecipes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false)
+    private User owner;
+    @ManyToOne
+    private Recipe recipe;
+}
