b67df1a0b609e6f5d470815bd65c5b2362b564a2
muminova_02 <muminova.2m@gmail.com>
2024-11-09 13:22:16 +0500

    some

--- src/main/java/uz/doublem/foodrecipe/config/MySecurityConfig.java
+++ src/main/java/uz/doublem/foodrecipe/config/MySecurityConfig.java
@@ -40,7 +40,7 @@ private UserRepository userRepository;
                        .cors((cr)-> cr.disable())
                        .userDetailsService(userDetailsService())
                        .authorizeRequests()
-                       .requestMatchers("")
+                       .requestMatchers("/api/")
                        .permitAll()
                        .requestMatchers(HttpMethod.POST)
                        .hasRole("USER")
