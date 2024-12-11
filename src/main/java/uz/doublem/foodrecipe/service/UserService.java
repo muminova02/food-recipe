e5666fadf4d0b9f63f650f1ad9bfb684e03b0bdf
muminova_02 <muminova.2m@gmail.com>
2024-12-12 01:10:01 +0500

    fixed city tashkent

--- src/main/java/uz/doublem/foodrecipe/service/UserService.java
+++ src/main/java/uz/doublem/foodrecipe/service/UserService.java
@@ -127,8 +127,8 @@ public class UserService {
                 .recipeNumber(countRecipe)
                 .followersCount(user.getFollowers_count())
                 .followingCount(user.getFollowing_count())
-                .country(user.getLocation().getCountry())
-                .country(user.getLocation().getCity())
+                .country(user.getLocation() == null ? "" : user.getLocation().getCountry())
+                .city(user.getLocation() == null ?  "" : user.getLocation().getCity())
                 .build();
         return getResponseMes(true,"user details for profile",build);
     }
